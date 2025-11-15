dc4448e3e9039eccf6dc32f097af8ed5dbae17b6
aniruddha-aits <aniruddha.aits@gmail.com>
2025-11-15 18:13:21 +0530

    Stocks Update

--- static/js/stocks.js
+++ static/js/stocks.js
@@ -1,8 +1,54 @@
+let stocks = [];
 
 function saveStock(event) {
     event.preventDefault();
 
     let stockName = document.getElementById('stockName').value;
+    let quantity = document.getElementById('quantity').value;
+    let price = document.getElementById('price').value;
+    let supplier = document.getElementById('supplier').value;
 
-    console.log(stockName);
+    stocks = [stockName, quantity, price, supplier];
+
+    let Table = document.getElementById("stockTable");
+
+    Table.innerHTML += `
+        <tr>
+            <td>${stockName}</td>
+            <td>${quantity}</td>
+            <td>${price}</td>
+            <td>${supplier}</td>
+        </tr>
+    `;
+
+    console.log(stocks);
+}
+
+function ClearForm() {
+
+    document.getElementById('stockName').value = "";
+    document.getElementById('quantity').value = "";
+    document.getElementById('price').value = "";
+    document.getElementById('supplier').value = "";
+
+    console.log("Modal Closed");
+
+}
+
+function searchStock(event) {
+    event.preventDefault();
+    let length = stocks.length;
+    let isFound = false;
+    let nameSearch = document.getElementById("search").value;
+
+    for (let i=0; i<length; i++) {
+        if (stocks[i] == nameSearch) {
+            console.log("Stock Found");
+            isFound = true;
+        }
+    }
+
+    if (isFound == false) {
+        console.log("Stock not found");
+    }
 }
